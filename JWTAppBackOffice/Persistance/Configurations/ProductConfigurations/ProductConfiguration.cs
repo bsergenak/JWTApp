d233a9a4d659cf79aed5b68ac1de007fed0709f3
bsergenak <berkaysergen.ak@gmail.com>
2023-04-19 02:46:45 +0300

    Repositoryler, role için gerekli olan enums configurationlar ve context eklenip migration atıldı.

---
+++ JWTAppBackOffice/Persistance/Configurations/ProductConfigurations/ProductConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using JWTAppBackOffice.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JWTAppBackOffice.Persistance.Configurations.ProductConfigurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(x=>x.Category).WithMany(x=>x.Products).HasForeignKey(x=>x.CategoryId);
+        }
+    }
+}
